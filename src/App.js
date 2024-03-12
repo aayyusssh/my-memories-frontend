e651caddafad134aed349e83464e44d359d67e7b
Ayush Mandal <ayush.mandal@corider.in>
2024-03-12 15:53:11 +0530

    updated from local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Container} from '@material-ui/core';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import {GoogleOAuthProvider} from '@react-oauth/google';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+import PostDetails from './components/PostDetails/PostDetails';
+
+
+
+const App = () =>{
+    const clientId = '268548838482-1jsla1kct3eakj1utbmrh3ve8ign8bkl.apps.googleusercontent.com'
+    const user = JSON.parse(localStorage.getItem('profile'));
+    return(
+        <GoogleOAuthProvider clientId={clientId}>
+        <BrowserRouter>
+        <Container maxWidth='xl'>
+            <Navbar/>
+            <Switch>
+                <Route path='/' exact component={()=> <Redirect to='/posts'/>}/>
+                <Route path='/posts' exact component={Home}/>
+                <Route path='/posts/search' exact component={Home}/>
+                <Route path='/posts/:id' exact component={PostDetails}/>
+                <Route path='/auth' exact component={()=>(!user?<Auth/>:<Redirect to='/posts'/>)}/>
+            </Switch>
+        </Container>
+        </BrowserRouter>
+        </GoogleOAuthProvider>
+    )
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,61 @@
+import React, {useState,useEffect} from 'react';
+import {Link, useHistory, useLocation} from 'react-router-dom';
+import {AppBar, Typography, Toolbar, Button, Avatar} from '@material-ui/core';
+import decode from 'jwt-decode';
+import {useDispatch} from 'react-redux'
+
+import { LOGOUT } from '../../constants/actionTypes';
+import useStyles from  './styles'
+import allMemoriesText from '../Images/ALL MEMORIES.svg';
+import allMemoriesLogo from '../Images/logo.png';
+
+
+const Navbar = () => {
+
+  const classes = useStyles();
+  const [user,setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+const dispatch = useDispatch()
+ const history = useHistory()
+const location = useLocation()
+
+  const logout = ()=>{
+      dispatch({type:LOGOUT});
+
+      history.push('/')
+      setUser(null);
+  }
+
+  useEffect(()=>{
+    
+    const token = user?.credential
+    
+    if(token){
+      const decodedToken = decode(token);
+
+      if(decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
+
+    setUser(JSON.parse(localStorage.getItem('profile')));
+
+  },[location])
+    return (
+        <AppBar className={classes.appBar} position='static' color='inherit'>
+          <Link to='/' className={classes.brandContainer}>
+        <img src={allMemoriesText} alt='icon' height="45px"/>
+        <img className={classes.image} src={allMemoriesLogo} alt='icon' height='40px'/>
+        </Link>    
+        <Toolbar className={classes.toolbar}>
+           {user ?(<div className={classes.profile}>
+                  <Avatar className={classes.purple} alt={user.result?.name} src={user.result?.picture}>{user.result?.name.charAt(0)}</Avatar>
+                   <Typography className={classes.userName} variant='h6'>{user.result?.name}</Typography>
+                   <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+           </div>):(
+            <Button component={Link} to='/auth' variant='contained' color='primary'>Sign In</Button>
+           )}
+        </Toolbar>
+    </AppBar>
+    );
+
+}
+
+export default Navbar
