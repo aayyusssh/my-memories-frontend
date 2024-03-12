3fdc94d83bef71946582185ca5401dca7c495d47
Ayush Mandal <ayush.mandal@corider.in>
2024-03-12 16:14:48 +0530

    Updates

---
+++ precache-manifest.8e987403e76ff65ed10caa3cd7f8f218.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7750d6f3aee8ef475071a53483d0f50",
+    "url": "/my-memories-frontend/index.html"
+  },
+  {
+    "revision": "0151fdbe12f3155f97be",
+    "url": "/my-memories-frontend/static/css/main.576bad8f.chunk.css"
+  },
+  {
+    "revision": "e51b754b0b25af5fe3a7",
+    "url": "/my-memories-frontend/static/js/2.0fa94e89.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/my-memories-frontend/static/js/2.0fa94e89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0151fdbe12f3155f97be",
+    "url": "/my-memories-frontend/static/js/main.5e9ee4df.chunk.js"
+  },
+  {
+    "revision": "a28c8d9b074d6818ebef",
+    "url": "/my-memories-frontend/static/js/runtime-main.db6f7dc8.js"
+  },
+  {
+    "revision": "10b858a24ff697c845c0a846264c19f6",
+    "url": "/my-memories-frontend/static/media/ALL MEMORIES.10b858a2.svg"
+  },
+  {
+    "revision": "7a63926722697d729cbec40a590881e8",
+    "url": "/my-memories-frontend/static/media/logo.7a639267.png"
+  }
+]);
